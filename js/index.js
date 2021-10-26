32f45c1d48ce7f8b97de7db5c416bb6b2457058e
dokkalemon <dottori.roberto@gmail..com>
2021-10-26 15:41:51 +0200

    start mail program

---
+++ js/index.js
@@ -0,0 +1,16 @@
+/* Mail
+- crea una lista contenente almeno 3 indirizzi email
+- chiedi all’utente la sua email,
+- controlla che sia nella lista di chi può accedere (lista creata sopra al punto 1),
+- stampa un messaggio appropriato sull’esito del controllo.
+ */
+
+
+
+
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Ricordate che la documentazione 
+è li per aiutarci: https://www.w3schools.com/js/js_random.asp 
+*/
